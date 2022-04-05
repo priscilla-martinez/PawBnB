1e1f6d568afeff918ea690e9a9756206cd4b6ca9
Priscilla Martinez <priscillamartinez@PriscillasMBP2.attlocal.net>
2022-04-04 23:38:32 -0700

    Basic header created and links to pages implemented

---
+++ react-frontend/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Route, Link, Routes, Navigate} from "react-router-dom"
+import './Home.css' 
+import Header from '../Header/Header'
+
+function Home() {
+  return (
+    <div>
+        <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ react-frontend/src/components/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Route, Link, Routes, Navigate} from "react-router-dom"
+
+function SignUp() {
+  return (
+    <div>
+        <h1>Sign Up Page</h1>
+    </div>
+  )
+}
+
+export default SignUp
